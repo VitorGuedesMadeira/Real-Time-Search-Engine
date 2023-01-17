f2318acfa6fa13a0811ccbd08c822ac3f7c340f1
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-01-17 19:06:08 -0300

    Add styles to login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :articles
-  resources :analytics
+
+  resources :articles, only: [:index :show]
+  resources :analytics, only: [:index]
 
   # Defines the root path route ("/")
   root "articles#index"
